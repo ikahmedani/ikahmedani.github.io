57941f00155d2ec20fca67c9b81e765f99b0d554
iahmedani <imran.ahmedani@gmail.com>
2019-02-02 20:28:40 +0500

    Update to replace any white space or symbols in dataset with underscore '_'

--- kobo1.js
+++ kobo1.js
@@ -39,12 +39,14 @@
       var x = [];
       for (var i = 0; i<data.length; i++){
           var y =  _.mapKeys(data[i], function(value, key) {
-              if(key.indexOf('/') == -1){
-                  return key
-              }else{
   
-                  return key.replace(/\//ig,'_')
-              }
+  
+              return key.replace(/\W/g,'_')
+              // if(key.indexOf('/') == -1){
+              //     return key
+              // }else{
+  
+              // }
             });
             x.push(y);
       }
@@ -136,14 +138,11 @@
       var yy = [];
       for (var i = 0; i < data.length; i++){
           for(var j = 0; j< uniqueVar.length; j++){
-              if(data[i][uniqueVar[j]] ){ 
-                  // //(uniqueVar[j])
-                  // //(yy.filter(function(el){el == uniqueVar[j]}).length)  
-                  
+              if(data[i][uniqueVar[j]] ){          
                   
                   if(yy.indexOf(uniqueVar[j]) == -1){                   
                       cols.push({
-                          id:uniqueVar[j].replace(/\//ig, '_'),
+                          id:uniqueVar[j].replace(/\W/g,'_'),
                           dataType: myType(data[i][uniqueVar[j]])
                       })
                       yy.push(uniqueVar[j])
@@ -153,11 +152,6 @@
               }
           }
       }
-      // //(uniqueVar, data)
-  //    //(cols)
-  //    var z = myUnique1(cols)
-  //    //(z)
-  // //(yy)
       cb(null, cols)
     
   }
@@ -225,42 +219,7 @@
           .catch(function(e){
               //(e)
           })
-  
-      // var cols = [
-      //   {
-      //     id: "id",
-      //     dataType: tableau.dataTypeEnum.string
-      //   },
-      //   {
-      //     id: "mag",
-      //     alias: "magnitude",
-      //     dataType: tableau.dataTypeEnum.float
-      //   },
-      //   {
-      //     id: "title",
-      //     alias: "title",
-      //     dataType: tableau.dataTypeEnum.string
-      //   },
-      //   {
-      //     id: "lat",
-      //     alias: "latitude",
-      //     dataType: tableau.dataTypeEnum.float
-      //   },
-      //   {
-      //     id: "lon",
-      //     alias: "longitude",
-      //     dataType: tableau.dataTypeEnum.float
-      //   }
-      // ];
-  
-      // var tableSchema = {
-      //   id: "earthquakeFeed",
-      //   alias:
-      //     "Earthquakes with magnitude greater than 4.5 in the last seven days",
-      //   columns: cols
-      // };
-  
-      // schemaCallback([tableSchema]);
+
     };
   
     // Download the data
@@ -272,14 +231,6 @@
       getDataForSchema(_userInputs)
       .then(function(result){
           return _formatDataPromise(result)
-          // tableData =[];
-          // for (var i =0; i<result.length; i++){
-          //     var x =  _.mapKeys(result[i], function(value, key) {
-          //         return key.replace(/\//ig,'_');
-          //       });
-          //       table.push(x)
-          // }
-  
           
       })
       .then(function(result){
@@ -287,31 +238,9 @@
           doneCallback()
       })
       .catch(function(e){
-          //(e)
+          console.log(e)
       })
   
-      // // //(JSON.parse(tableau.connectionData));
-      // $.getJSON(
-      //   "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson",
-      //   function(resp) {
-      //     var feat = resp.features,
-      //       tableData = [];
-  
-      //     // Iterate over the JSON object
-      //     for (var i = 0, len = feat.length; i < len; i++) {
-      //       tableData.push({
-      //         id: feat[i].id,
-      //         mag: feat[i].properties.mag,
-      //         title: feat[i].properties.title,
-      //         lon: feat[i].geometry.coordinates[0],
-      //         lat: feat[i].geometry.coordinates[1]
-      //       });
-      //     }
-  
-      //     table.appendRows(tableData);
-      //     doneCallback();
-      //   }
-      // );
     };
   
     tableau.registerConnector(myConnector);
